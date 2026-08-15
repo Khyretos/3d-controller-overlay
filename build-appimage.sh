6b5b4ec42098f34bf475554d12e33c373170591c
Khyretos <kees.rodriguez@gmail.com>
2026-08-16 00:14:01 +0200

    renaming cleanup

--- build-appimage.sh
+++ build-appimage.sh
@@ -5,4 +5,4 @@ echo "🔨 Building Linux AppImage (statically linked)..."
 docker build -t 3dco-appimage-builder -f Dockerfile.appimage .
 mkdir -p dist
 docker run --rm -v $(pwd)/dist:/dist 3dco-appimage-builder
-echo "✅ AppImage created in ./dist/3dco.AppImage"
+echo "✅ AppImage created in ./dist/3dco+.AppImage"
